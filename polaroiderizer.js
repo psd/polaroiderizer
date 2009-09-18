c080ff737cf26d9280538197e118f28b0f81a02b
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2009-09-18 17:06:00 +0100

    refactored back into two .js files, and to satisfy jslint

--- polaroiderizer.js
+++ polaroiderizer.js
@@ -1,4 +1,4 @@
-/*
+    /*
  * Polaroiderizer - slideshow from feeds using jQuery
  *
  * Dual licensed under the MIT and GPL licenses:
@@ -6,71 +6,178 @@
  *   http://www.gnu.org/licenses/gpl.html
  *
  */
-;(function($) {
 
-$.fn.polaroiderizer = function(options) {
-	var defaults = {};
-	var settings = $.extend(true, defaults, options);
+/*jslint browser: true, onevar: false */
+/*global window, escape, jQuery */
 
-	// handle the form submission.
-	$('#form').submit(function(){
-		var query = $('#query').val();
+(function ($) {
 
-		// set the fragment identifier
-		window.location.hash = '#' + query;
+    /*
+     *  background tasks
+     */
+    var running = false;
 
-		// kick off display queue
-		displayQueue.start();
-		
-		// kick off feed queue
-		feedQueue.start({queue: displayQueue, query: query});
+    function runFeeds() {
+        var query = $('#query').val();
+        $.whileAsync({
+            delay: 300000,
+            bulk: 0,
+            loop: function () {
+                $.fn.polaroidizer.feed.flickr(query); 
+            } 
+        });
+    }
 
-		return false;
-	});
+    function runDisplay() {
+        $.whileAsync({
+            delay: 3000,
+            bulk: 0,
+            loop: function () {
+                console.log("display");
 
-	// get the fragment identifier
-	if(window.location.hash) {
-		$('#query').val(window.location.hash.split('#')[1]);
-		$('form').submit();
-	}
+                var item = $('#staging .ready(0)');
+                if (!item) {
+                    return;
+                }
+                $(item).transition();
 
-	// fullscreen UI selector
-	$('a.toggleFullscreen').click(function(){
-		toggleFullscreen();
-	});
-	$().keypress(function(e){
-		if(!$(e.target).is('#query')) {
-			if(e.which == 102) {
-				toggleFullscreen();
-			}
-		}
-	});
+                // update status
+                var title = $(item).find('img').attr('title');
+                var link = $(item).clone().empty().text(title);
+                $('#status').html(link);
+            }
+        });
+    }
 
-	// transition UI selector
-	$('#options a').click(function(){
-		var target = $(this);
-		if(!target.hasClass('selected')) {
-			$('#options a').removeClass('selected');
-			target.addClass('selected');
-		}
-		target.blur();
-		return false;
-	});
-};
+    function runTasks() {
+        if (running) {
+            return;
+        }
+        running = true;
+    }
 
+    /*
+     *  UI
+     */
+    function toggleFullscreen(argument) {
+        var display = $('#display');
+        $('#title, #form, #controls, #footer').toggle();
+        $('body').toggleClass('kiosk');
+        display.toggleClass('kiosk');
+        if (display.hasClass('kiosk')) {
+            var h = $(window).height() + 'px';
+            display.height(h);
+        } else {
+            display.height('500px');
+        }
+    }
 
-// toggle fullscreen with the f key or a click.	
-function toggleFullscreen (argument) {
-	var display = $('#display');
-	$('#title, #form, #controls, #footer').toggle();
-	$('body').toggleClass('kiosk');
-	display.toggleClass('kiosk');
-	if(display.hasClass('kiosk')) {
-		var h = $(window).height() + 'px';
-		display.height(h);		
-	} else {
-		display.height('500px');				
-	}
-}
+    /*
+     *  plugin initialisation
+     */
+    $.fn.polaroiderizer = function (options) {
+        var defaults = {},
+            settings = $.extend(true, defaults, options);
 
-})(jQuery);
+        // handle the form submission.
+        $('#form').submit(function () {
+            var query = $('#query').val();
+            window.location.hash = '#' + query;
+
+            $('#staging').empty();
+
+            runTasks();
+            return false;
+        });
+
+        // get the fragment identifier
+        if (window.location.hash) {
+            $('#query').val(window.location.hash.split('#')[1]);
+            $('form').submit();
+        }
+
+        // fullscreen UI selector
+        $('a.toggleFullscreen').click(function () {
+            toggleFullscreen();
+        });
+        
+        $().keypress(function (e) {
+            if (!$(e.target).is('#query')) {
+                if (e.which === 102) {
+                    toggleFullscreen();
+                }
+            }
+        });
+
+        // transition UI selector
+        $('#options a').click(function () {
+            var target = $(this);
+            if (!target.hasClass('selected')) {
+                $('#options a').removeClass('selected');
+                target.addClass('selected');
+            }
+            target.blur();
+            return false;
+        });
+    };
+
+    $.fn.polaroiderizer.statusMessage = function (message) {
+        $('#status').html(message);
+    };
+
+    $.fn.polaroiderizer.add = function (item) {
+        $('<img src="' + item.profile_image_url + '" title="' + item.from_user + '">')
+            .load(function () {
+                this.addClass("ready");
+            })
+            .appendTo('#staging');
+    };
+
+    // flickr search feed
+    $.fn.polaroiderizer.feed.flickr = function (text) {
+        this.statusMessage('checking flickr for photo s' + text + '...');
+        var nphotos = 5;
+        var api_key = '0a346a54dbca829015b11fcac9e70c6f';
+
+        var uri = 'http://api.flickr.com/services/rest/?method=flickr.photos.search' +
+            '&api_key=' + api_key +
+            '&text=' + escape(text) +
+            '&per_page=' + nphotos +
+            '&format=json' +
+            '&jsoncallback=?';
+
+        $.getJSON(uri, function (data) {
+            $.each(data.photos.photo, function (i, item) {
+                $.fn.polaroiderizer.add({
+                    type: 'photo', 
+                    user: item.owner,
+                    title: item.title, 
+                    text: item.description,
+                    img: {
+                        src: 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.s,
+                        href: 'http://flickr.com/photos/' + item.owner + '/' + item.id
+                    }
+                });
+            });
+        });
+    };
+
+    // twitter search feed
+    $.fn.polaroiderizer.feed.twitter = function (text) {
+        this.statusMessage('searching twitter for tweets ...');
+
+        var uri = 'http://search.twitter.com/search.json?q=' + escape(text) + '&callback=?';
+
+        $.getJSON(uri, function (data) {
+            $.each(data.results, function (i, item) {
+                $.fn.polaroiderizer.add({
+                    type: 'tweet', 
+                    user: item.from_user,
+                    avatar: item.profile_image_url, 
+                    title: item.from_user, 
+                    text: item.text
+                });
+            });
+        });
+    };
+}(jQuery));
--- polaroiderizer.transition.js
+++ polaroiderizer.transition.js
@@ -7,93 +7,100 @@
  *
  */
 
-// choose a transition effect
-$.fn.transition = function() {
-	var effect = $('#options a.selected').text();
-	if(effect == 'polaroids'){
-		this.polaroidScroll();
-	} else if(effect == 'faders'){
-		this.faders();	
-	} else if(effect == 'plain'){
-		this.plain();	
-	}	
-	return this;
-};
+/*jslint browser: true, onevar: false */
+/*global jQuery */
 
-$.fn.polaroidScroll = function() {
-	
-	$('#display div.plain').remove();
-				
-	var frame = $('<div class="polaroid"></div>').append(this);
-	var photo = $(this).find('img');
+(function ($) {
 
-	$('#display').append(frame);
-		
-	// resize the photo to fit the viewport
-	if(photo.height() > 400) {
-		photo.css({height:photo.height()*0.75, width:photo.width()*0.75});
-	}
-		
-	// set starting point
-	var x = 40 + Math.floor(Math.random() * ($('#display').width() - frame.width() - 80));
-	var y = frame.height();
-	frame.css({top: '-'+y+'px', left: x+'px'});
+    // choose a transition effect
+    $.fn.transition = function () {
+        var effect = $('#options a.selected').text();
+        if (effect == 'polaroids') {
+            this.polaroidScroll();
+        } else if (effect == 'faders') {
+            this.faders();	
+        } else if (effect == 'plain') {
+            this.plain();	
+        }	
+        return this;
+    };
 
-	// set opacity of photo
-	photo.css({opacity: '0'});
+    $.fn.polaroidScroll = function () {
+        
+        $('#display div.plain').remove();
+                    
+        var frame = $('<div class="polaroid"></div>').append(this);
+        var photo = $(this).find('img');
 
-	// animate photo opacity and into view.
-	frame.animate({top:'15px'}, 400);
-	photo.animate({opacity: '1'}, 2000, function(picture){
-		// animate slowly out of view and opacity of entire object.
-		frame.animate({top:$('#display').height()+'px', opacity:'0'}, 5000, function(){
-			frame.remove();
-		});	
-	});
-	return this;				
-};
+        $('#display').append(frame);
+            
+        // resize the photo to fit the viewport
+        if (photo.height() > 400) {
+            photo.css({height: photo.height()*0.75, width:photo.width()*0.75});
+        }
+            
+        // set starting point
+        var x = 40 + Math.floor(Math.random() * ($('#display').width() - frame.width() - 80));
+        var y = frame.height();
+        frame.css({top: '-'+y+'px', left: x+'px'});
 
-// fading animation effect
-$.fn.faders = function() {
-	
-	$('#display div.plain').remove();
-		
-	var frame = $('<div class="fader"></div>').append(this);
-	var photo = $(this).find('img');
+        // set opacity of photo
+        photo.css({opacity: '0'});
 
-	$('#display').append(frame);
-	
-	// resize the photo to fit the viewport
-	if(photo.height() > 400) {
-		photo.css({height:photo.height()*0.75, width:photo.width()*0.75});
-	}
-	
-	var x = ($('#display').width() - frame.width()) /2;
-	var y = ($('#display').height() - frame.height()) /2;
-	frame.css({top: y+'px', left: x+'px', opacity: '0'});
-	frame.animate({opacity: '1'}, 2000, function(pic){
-		frame.animate({opacity:'0'}, 3000, function(){
-			frame.remove();
-		});				
-	});
-	return this;				
-};
+        // animate photo opacity and into view.
+        frame.animate({top:'15px'}, 400);
+        photo.animate({opacity: '1'}, 2000, function (picture) {
+            // animate slowly out of view and opacity of entire object.
+            frame.animate({top:$('#display').height()+'px', opacity:'0'}, 5000, function () {
+                frame.remove();
+            });	
+        });
+        return this;				
+    };
 
-// simplest.effect
-$.fn.plain = function() {
+    // fading animation effect
+    $.fn.faders = function () {
+        
+        $('#display div.plain').remove();
+            
+        var frame = $('<div class="fader"></div>').append(this);
+        var photo = $(this).find('img');
 
-	$('#display div.plain').remove();
-	var frame = $('<div class="plain"></div>').append(this);
-	$('#display').append(frame);
-	
-	// resize the photo to fit the viewport
-	var photo = $(this).find('img');
-	if(photo.height() > 400) {
-		photo.css({height:photo.height()*0.75, width:photo.width()*0.75});
-	}
-	
-	var x = ($('#display').width() - frame.width()) /2;
-	var y = ($('#display').height() - frame.height()) /2;
-	frame.css({top: y+'px', left: x+'px'});
-	return this;				
-};
+        $('#display').append(frame);
+        
+        // resize the photo to fit the viewport
+        if (photo.height() > 400) {
+            photo.css({height:photo.height()*0.75, width:photo.width()*0.75});
+        }
+        
+        var x = ($('#display').width() - frame.width()) /2;
+        var y = ($('#display').height() - frame.height()) /2;
+        frame.css({top: y+'px', left: x+'px', opacity: '0'});
+        frame.animate({opacity: '1'}, 2000, function (pic) {
+            frame.animate({opacity:'0'}, 3000, function () {
+                frame.remove();
+            });				
+        });
+        return this;				
+    };
+
+    // simplest.effect
+    $.fn.plain = function () {
+
+        $('#display div.plain').remove();
+        var frame = $('<div class="plain"></div>').append(this);
+        $('#display').append(frame);
+        
+        // resize the photo to fit the viewport
+        var photo = $(this).find('img');
+        if (photo.height() > 400) {
+            photo.css({height:photo.height()*0.75, width:photo.width()*0.75});
+        }
+        
+        var x = ($('#display').width() - frame.width()) /2;
+        var y = ($('#display').height() - frame.height()) /2;
+        frame.css({top: y+'px', left: x+'px'});
+        return this;				
+    };
+
+}(jQuery));
