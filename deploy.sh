3feb165094d40ab29dbc13b4847922b300433c84
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2009-10-02 15:15:57 +0100

    deployed to test

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 user=ripmaster@bytenight.osmosoft.com
-dir=/data/vhost/bytenight.osmosoft.com/html/
 
+dir=/data/vhost/bytenight.osmosoft.com/html/
 scp moderated.php $user:$dir/moderated/index.php
-exit
 
 dir=/data/vhost/bytenight.osmosoft.com/html/test
 
-#ssh $user mkdir $dir $dir/plugins
+###ssh $user mkdir $dir $dir/plugins
 
-#scp plugins/jquery.async.js plugins/date.js $user:$dir/plugins
-#scp polaroiderizer.flickr.js polaroiderizer.js polaroiderizer.transition.js polaroiderizer.twitter.js $user:$dir
-#scp styles.css $user:$dir
+scp plugins/jquery.async.js plugins/date.js $user:$dir/plugins
+scp polaroiderizer.flickr.js polaroiderizer.js polaroiderizer.transition.js polaroiderizer.twitter.js $user:$dir
+scp styles.css $user:$dir
 
-#scp join-us.html $user:$dir
+scp join-us.html $user:$dir
 
-#scp bytenight.html $user:$dir/index.html
-#scp holding.html $user:$dir/index.html
+scp bytenight.html $user:$dir/index.html
+scp holding.html $user:$dir/index.html
