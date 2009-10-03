92df6d60de99652356c793f21491ccaaa2b809f5
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2009-10-03 14:28:13 +0100

    comment

--- moderated.php
+++ moderated.php
@@ -1,6 +1,9 @@
 <?php
-        $callback = isset($_GET['callback']) ? $_GET['callback'] : 'moderate';
-        echo $callback . '({"results":[' ;
-        echo '{ "id": "flickr_3974518474", blocked: "1" }';
-        echo ']});'
+    /*
+     *  return JSONP list of items to be blocked, a quick and dirty moderator list
+     */
+    $callback = isset($_GET['callback']) ? $_GET['callback'] : 'moderate';
+    echo $callback . '({"results":[' ;
+    echo '{ "id": "flickr_3974518474", blocked: "1" }';
+    echo ']});'
 ?>
