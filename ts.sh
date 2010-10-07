d761bca029fbfb807e2e1dd4c1c1462932ec61a4
Paul Downey <pauldowney@Paul-Downeys-Macbook-Air.local>
2010-10-07 16:27:43 +0100

    preparing for TiddlySpace version

---
+++ ts.sh
@@ -0,0 +1,17 @@
+#
+#  deploy to http://polaroiderizer.tiddlyspace.com
+#  depends upon 'sts': http://gist.github.com/604709
+#
+space=polaroiderizer
+
+sts $space polaroiderizer.html
+sts $space styles.css
+
+sts $space polaroiderizer.flickr.js
+sts $space polaroiderizer.js
+sts $space polaroiderizer.moderated.js
+sts $space polaroiderizer.transition.js
+sts $space polaroiderizer.twitter.js
+sts $space plugins/date.js
+sts $space plugins/jquery.async.js
+sts $space plugins/ordered.js
